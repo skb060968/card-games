3dd5f72a0048f0544e3af926a5fe4ec5bee985f5
Sunil <skb060968@gmail.com>
2026-05-12 16:55:12 +0530

    final

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@
    - Update detection + prompt support
    ============================== */
 
-const CACHE_NAME = "card-games-v16";
+const CACHE_NAME = "card-games-v17";
 
 // Pre-cache truly static assets (not Vite-hashed JS/CSS)
 const STATIC_ASSETS = [
