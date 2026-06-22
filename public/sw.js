8ef443702f1effb2d659a21df0239f9fdefa2db6
Sunil <skb060968@gmail.com>
2026-06-22 22:31:25 +0530

    Bump to v78 to avoid cache conflicts with broken v75-77 versions

--- public/sw.js
+++ public/sw.js
@@ -5,7 +5,7 @@
    - Update detection + prompt support
    ============================== */
 
-const CACHE_NAME = "card-games-v75";
+const CACHE_NAME = "card-games-v78";
 
 // Pre-cache truly static assets (not Vite-hashed JS/CSS)
 const STATIC_ASSETS = [
